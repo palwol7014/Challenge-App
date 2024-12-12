757f33a0cff8d75ca38c77e3ff748f0ba042298b
Paweł Wolnarek <wolnarek.pawel@gmail.com>
2024-12-12 14:52:35 +0100

    Day 4

--- Challenge App/Program.cs
+++ Challenge App/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var age = 30;
+var name = "Marta";
+var sex = 'K';
+string message;
+
+if((sex == 'K' || sex == 'k') && age < 30)
+{
+	message = "Kobieta poniżej 30 lat";
+}
+else if(name == "Ewa" && age == 30)
+{
+	message = $"{name}, lat {age}";
+}
+else if ((sex == 'm' || sex == 'M') && age < 18)
+{
+	message = "Niepełnoletni mężczyzna";
+}
+else
+{
+	message = "Brak klasyfikacji";
+}
+
+Console.WriteLine(message);
