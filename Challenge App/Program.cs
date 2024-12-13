0c291a9a57a99970a74a2d58fe0f6bcbef0a8a7f
Paweł Wolnarek <wolnarek.pawel@gmail.com>
2024-12-13 05:27:46 +0100

    Day 5

--- Challenge App/Program.cs
+++ Challenge App/Program.cs
@@ -1,23 +1,15 @@
-﻿var age = 30;
-var name = "Marta";
-var sex = 'K';
-string message;
+﻿var number = 455541101387996544;
+var numberOfString = number.ToString();
+var countOfDigits = new int[10];
 
-if((sex == 'K' || sex == 'k') && age < 30)
+foreach (var character in numberOfString)
 {
-	message = "Kobieta poniżej 30 lat";
-}
-else if(name == "Ewa" && age == 30)
-{
-	message = $"{name}, lat {age}";
-}
-else if ((sex == 'm' || sex == 'M') && age < 18)
-{
-	message = "Niepełnoletni mężczyzna";
-}
-else
-{
-	message = "Brak klasyfikacji";
+	//Bez względu na kodowanie znaków zawsze znaki cyfr 0 - 9 występują kolejno po sobie.
+	//'0' jest traktowany jako kod znaku.
+	countOfDigits[character - '0']++;
 }
 
-Console.WriteLine(message);
+for (var i = 0; i < countOfDigits.Length; i++)
+{
+	Console.WriteLine($"{i} => {countOfDigits[i]}");
+}
